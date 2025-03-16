128735ff7fadb4c2269463c39058eabe1440749e
Shraddha123634 <shraddhapanhale27406@gmail.com>
2025-03-16 17:08:26 +0530

    added changes

--- application/views/about.php
+++ application/views/about.php
@@ -19,7 +19,7 @@
                     <a class="nav-link active" aria-current="page" href="<?php echo URL ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="<?php echo URL.'about' ?>">About Us</a>
+                    <a class="nav-link" href="<?php echo URL.'about' ?>">About</a>
                 </li>
             </ul>
             </div>
