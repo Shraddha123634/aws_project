e0b433ac188159af0d9323ee5cd9f6867d1d14c5
Shraddha123634 <shraddhapanhale27406@gmail.com>
2025-04-12 22:02:44 +0530

    added changes

--- application/hooks/Cors.php
+++ application/hooks/Cors.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Cors {
     public function enableCors() {
+        // Allow both www and non-www
         $allowed_origins = [
             'https://trackerzz.club',
             'https://www.trackerzz.club'
@@ -10,15 +11,13 @@ class Cors {
 
         $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
 
-        print_r($origin); exit;
-
         if (in_array($origin, $allowed_origins)) {
             header("Access-Control-Allow-Origin: $origin");
             header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-            header("Access-Control-Allow-Headers: Content-Type, Authorization");
+            header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+            header("Access-Control-Allow-Credentials: true");
         }
 
-        // For preflight OPTIONS request
         if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
             header('HTTP/1.1 200 OK');
             exit();
