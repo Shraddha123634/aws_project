54bf871eeefb395fd4f5ec1b69731f0832e488b9
Shraddha123634 <shraddhapanhale27406@gmail.com>
2025-04-12 22:13:44 +0530

    added changes

--- application/controllers/Crud.php
+++ application/controllers/Crud.php
@@ -7,26 +7,6 @@ class Crud extends CI_Controller {
     {
         parent::__construct();
 
-		// Manually set CORS headers
-		$allowed_origins = [
-			'https://trackerzz.club',
-			'https://www.trackerzz.club'
-		];
-
-		$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
-
-		if (in_array($origin, $allowed_origins)) {
-			header("Access-Control-Allow-Origin: $origin");
-			header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-			header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
-			header("Access-Control-Allow-Credentials: true");
-		}
-
-		if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-			header('HTTP/1.1 200 OK');
-			exit();
-		}
-
         $this->load->model("Crud_model");
     }
 
--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
+
+$origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
+
+$allowed_origins = [
+    'https://www.trackerzz.club'
+];
+
+if (in_array($origin, $allowed_origins)) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Credentials: true");
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+// (leave the rest of index.php as it is)
+
+
+
 /**
  * CodeIgniter
  *
