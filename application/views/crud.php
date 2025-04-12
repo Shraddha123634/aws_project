91e742a8fba2ff5a48b9ec6e73d0f59e26624d2f
Shraddha123634 <shraddhapanhale27406@gmail.com>
2025-04-12 14:57:03 +0530

    added changes

--- application/views/crud.php
+++ application/views/crud.php
@@ -57,7 +57,7 @@
                                 <th scope="row"><?php echo ($key + 1) ?></th>
                                 <td><?php echo !empty($list['email']) ? $list['email'] : '' ?></td>
                                 <td><?php echo !empty($list['password']) ? $list['password'] : '' ?></td>
-                                <td><a type="button" class="btn btn-danger" onclick="deleteEntry(<?php echo $list['id'] ?>)" >Delete</a></td>
+                                <td><button type="button" class="btn btn-danger" onclick="deleteEntry(<?php echo $list['id'] ?>)" >Delete</button></td>
                             </tr>
                         <?php } ?>
                         </tbody>
